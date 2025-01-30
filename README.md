91ad28ed66c0805c96946d3ba46396c3408284b2
Jasur <jasurbekshomaqsudov2004@gmail.com>
2025-01-30 09:18:13 +0500

    minimized codes and fixed docs

--- README.md
+++ README.md
@@ -1,21 +1,17 @@
 # isMobileDevice Function
-
 This package provides the `isMobileDevice` utility function that determines whether the current device is a mobile device based on the `userAgent` string. It checks various user agents to identify devices like Android, iOS, BlackBerry, and other mobile operating systems.
 
 ## Installation
-
 To install this package, run the following command:
-
 ```bash
 npm install mobile_device_checker
 ```
 
 ## Usage
-
 ```typescript
-import { isMobileDevice } from "moblie_device_checker";
+import isMobileDevice from "moblie_device_checker";
 
-// Check if the device is mobile
+//Check if the device is mobile
 const result = isMobileDevice();
-console.log(result); // true if mobile device, false otherwise
+console.log(result);//true if mobile device, false otherwise
 ```
--- main.ts
+++ main.ts
@@ -1,21 +1,16 @@
 /**
- * Checks if the current device is a mobile device based on the user agent string.
- *
- * This function examines the `userAgent`, `vendor`, and `opera` properties from the `navigator` object to determine
- * whether the device is a mobile device such as Android, iPhone, iPad, or other mobile OS types.
- *
- * ## Usage
- *
- * ```typescript
- * import { isMobileDevice } from "mobile_device_checker";
- *
- * // Check if the device is mobile
- * const result = isMobileDevice();
- * console.log(result); // true if mobile device, false otherwise
- * ```
- *
- * @returns {boolean} - Returns `true` if the device is a mobile device (Android, iOS, etc.), `false` otherwise.
- */
+*Checks if the current device is a mobile device based on the user agent string.
+*
+*This function examines the `userAgent`, `vendor`, and `opera` properties from the `navigator` object to determine whether the device is a mobile device such as Android, iPhone, iPad, or other mobile OS types.
+*## Usage
+*```typescript
+*import { isMobileDevice } from "mobile_device_checker";
+*
+* // Check if the device is mobile
+*const result = isMobileDevice();
+*console.log(result); // true if mobile device, false otherwise
+*```
+*@returns {boolean} Returns `true` if the device is a mobile device (Android, iOS, etc.), `false` otherwise.*/
 export default function isMobileDevice():boolean{
   const uA:string=navigator?.userAgent||navigator?.vendor||(window as any)?.opera||"";
   return uA?/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(uA?.toLowerCase()):false;
